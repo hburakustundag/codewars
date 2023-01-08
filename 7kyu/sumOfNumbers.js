5930cce3e1de3d734a7521ca52b1561c5f654859
Halit Burak Ustundag <hburakustundag@gmail.com>
2023-01-08 13:51:52 +0300

    Create sumOfNumbers.js

---
+++ 7kyu/sumOfNumbers.js
@@ -0,0 +1,13 @@
+function getSum(a, b) {
+  if (a === b) {
+    return a;
+  }
+  if (a > b) {
+    [a, b] = [b, a];
+  }
+  let sum = 0;
+  for (let i = a; i <= b; i++) {
+    sum += i;
+  }
+  return sum;
+}
